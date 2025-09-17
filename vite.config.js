117a3dc46dcb676bccaabb604299034938954957
bekatev <74668742+bekatev@users.noreply.github.com>
2025-09-18 01:33:37 +0400

    chore: firebase hosting config + dynamic vite base

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
+// Allow switching base between GitHub Pages and Firebase
+const DEPLOY_TARGET = process.env.DEPLOY_TARGET || ''
+
 export default defineConfig({
   plugins: [vue()],
-  // Base path for GitHub Pages under /b-rational/
-  base: '/b-rational/',
+  base: DEPLOY_TARGET === 'github' ? '/b-rational/' : '/',
 })
